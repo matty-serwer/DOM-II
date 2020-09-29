1646356dbfa854cc0e8a0da735870a81fff8a0a4
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-09-29 16:37:51 -0400

    added even listeners mouseover and mouseout to the funBus img. added keydown and keyup listeners for the spacebar. added wheel event listener for the map img. added focus and blur even listeners to the document

--- js/index.js
+++ js/index.js
@@ -1,1 +1,49 @@
-// Your code goes here
+const funBus = document.querySelectorAll('img')[0];
+const map = document.querySelectorAll('img')[1];
+const body = document.querySelector('body');
+const allPara = document.querySelectorAll('p');
+const nav = document.querySelector('nav');
+const navContainer = document.querySelector('.nav-container');
+const header = document.querySelector('header');
+
+funBus.addEventListener('mouseover', () => {
+    body.classList.add('blue');
+})
+
+funBus.addEventListener('mouseout', () => {
+    body.classList.remove('blue');
+})
+
+document.addEventListener('keydown', (event) => {
+    // console.log(event);
+    if(event.code === "Space") {
+        allPara.forEach(para => {
+            para.classList.add('bigText');
+        })
+        event.preventDefault();
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    // console.log(event);
+    if(event.code === "Space") {
+        allPara.forEach(para => {
+            para.classList.remove('bigText');
+        })
+        event.preventDefault();
+    }
+})
+
+map.addEventListener('wheel', (event) => {
+    // console.log(event);
+    map.classList.add('tinyPicture');
+})   
+
+window.addEventListener('focus', (event) => {
+    console.log(event);
+    navContainer.classList.add('pink');
+})
+
+window.addEventListener('blur', () => {
+    navContainer.classList.remove('pink');
+})
